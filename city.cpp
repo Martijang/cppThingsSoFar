1230a4f325ccd9952dc92017e8b22d79063ba1c8
Martijang <seungwoogo1123@gmail.com>
2025-09-26 09:16:49 +0800

    Update city.cpp
    Code style changing.

--- city.cpp
+++ city.cpp
@@ -4,33 +4,33 @@
 
 class City{
     private:
-    std::unordered_map<int, std::string> Map;
+    std::unordered_map<int, std::string> m_Map;
     public:
     // adds new citizen, requirements: id(int) comment(std::stringi)
     void Add(int &id, std::string &extra){
 
-        if(Map.find(id) != Map.end()){
+        if(m_Map.find(id) != m_Map.end()){
             std::cout << "This id already exists\n";
         }else{
-            Map[id] = extra;
+            m_Map[id] = extra;
             std::cout << "ID: " << id << " COMMENT: " << extra << " has been added\n";
         }
     }
     // deletes citizen, requirements: id(int)
     void Delete(int &id){
 
-        if(Map.find(id) != Map.end()){
-            std::cout << "ID: " << id << " COMMENT: " << Map.find(id)->second << " has been deleted\n";
-            Map.erase(id);
+        if(m_Map.find(id) != m_Map.end()){
+            std::cout << "ID: " << id << " COMMENT: " << m_Map.find(id)->second << " has been deleted\n";
+            m_Map.erase(id);
         }else{
             std::cout << "ID: " << id << " does not exists\n";
         }
     }
 
-    //prints all items in the Map
-    void Print_all(){
+    //prints all items in the map
+    void PrintAll(){
         int total = 0;
-        for (auto& [key, value] : Map)
+        for (auto& [key, value] : m_Map)
         {
             std::cout << "ID: " << key << " COMMENT: " << value << std::endl;
             total ++;
@@ -40,8 +40,8 @@ class City{
 
     //Looking for specific citizen, requirements: id(int)
     void Look(int id){
-        if(Map.find(id) != Map.end()){
-            std::cout << "ID: " << id << " COMMENT: " << Map.find(id)->second << std::endl;
+        if(m_Map.find(id) != m_Map.end()){
+            std::cout << "ID: " << id << " COMMENT: " << m_Map.find(id)->second << std::endl;
         }else{
             std::cout << "ID: " << id << " does not exists\n";
         }
@@ -82,7 +82,7 @@ int main(){
 
             ct.Delete(id);
         }else if(input == "print all"){
-            ct.Print_all();
+            ct.PrintAll();
         }else if(input == "Look"){
             int id;
 
